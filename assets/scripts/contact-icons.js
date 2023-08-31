a682ab2c2049b40f60184ef6378193a65b68b8c5
Sasha <sashavikhorevaa@gmail.com>
2023-08-31 21:59:29 +0300

    fix : fix float icons

--- assets/scripts/contact-icons.js
+++ assets/scripts/contact-icons.js
@@ -1,5 +1,6 @@
 let whats = document.querySelector(".whats");
 let mail = document.querySelector(".mail");
+let up = document.querySelector(".up");
 
 window.onscroll = function () {
   scrollFunction();
@@ -12,19 +13,26 @@ function scrollFunction() {
   ) {
     whats.style.display = "block";
     mail.style.display = "block";
+    up.style.display = "block";
   } else {
     whats.style.display = "none";
     mail.style.display = "none";
+    up.style.display = "none";
   }
   if(window.innerWidth <= 480){
     mail.style.display = "none";
   }
 }
 
+
+
 $('body').on('click', '.whats', function(e){
   window.open('http://wa.me/79037293137','_blank');
 })
 $('body').on('click', '.mail', function(e){
   document.location.href = 'mailto:support@digsol.tech?subject=Заявка с сайта';
-  // window.open('mailto:support@digsol.tech?subject=Заявка с сайта');
+})
+$('body').on('click', '.up', function(e){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 })
